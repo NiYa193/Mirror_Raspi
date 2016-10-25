c9dbb5b0e6bf00d18836af95d4e0626757c61278
NiYa193 <1556579665@qq.com>
2016-10-25 13:25:16 +0800

    Reload the code

--- README.md
+++
@@ -1,3 +0,0 @@
-The mirror is buildding now.
-Please wait.
-				---FCC
--- pi/blink.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pin = $_GET['pin'];
-echo "pin:$pin, blink";
-system("gpio write $pin 1");
-system("gpio write $pin 0");
-?>
